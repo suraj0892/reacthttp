7989f05a78792b90e9d631cfd4921c283af4badd
suraj <surajprabakark@gmail.com>
2022-01-08 20:15:56 +0530

    use context hook with default and custom values

--- src/App.js
+++ src/App.js
@@ -11,10 +11,19 @@ import UseEffectOnce from './Hooks/useEffectOnce';
 import MouseContainer from './Hooks/MouseContainer';
 import UseEffectWithIncorrectDepedency from './Hooks/UseEffectWithIncorrectDepedency';
 import UseEffectWithHttpGet from './Hooks/UseEffectWithHttpGet';
+import React from 'react';
+import UseContextExample from './Hooks/UseContextExample';
+
+export const UserContext = React.createContext("Suraj")
+export const ChannelContext = React.createContext("Channle")
+
 function App() {
   return (
     <div className="App">
-      <UseEffectWithHttpGet />
+      <UserContext.Provider value="Dumeel">
+      <UseContextExample/>
+      </UserContext.Provider>
+      {/* <UseEffectWithHttpGet /> */}
       {/* <UseEffectWithIncorrectDepedency/> */}
       {/* <MouseContainer />  */}
       {/* <UseEffectOnce /> */}
